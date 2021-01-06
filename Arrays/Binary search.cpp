aa96f16d029e8a538851554fcd9c8e0cc1a5d5e8
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-01-06 21:46:13 +0530

    Create Binary search.cpp

---
+++ Arrays/Binary search.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int binary_search(int arr[], int n, int key)
+{
+	int s=0;
+	int e=n-1;
+	while(s<=e)
+	{
+		int mid=(s+e)/2;
+		if(arr[mid] == key)
+		{
+			return mid;
+			
+		}
+		else if(arr[mid] < key)
+		{
+			s=mid+1;
+		}
+		else{
+			e=mid-1;
+		}
+	}
+		return -1;
+}
+int main() {
+	int n;
+	cin>>n;
+	int arr[10000];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	int key;
+	cin>>key;
+	
+	cout<<binary_search(arr,n,key)<<endl;
+	return 0;
+}

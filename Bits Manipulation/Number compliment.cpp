b938a573508721170ca608a43626ed8d53d11040
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-04-22 21:56:17 +0530

    Create Number compliment.cpp

---
+++ Bits Manipulation/Number compliment.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findComplement(int num) {
+       long long c=0,n=num;
+        while(num){
+            c++;
+            num>>=1;
+        }
+        return (int) (n^ ((long long)(1<<c)-1));
+    }
+};

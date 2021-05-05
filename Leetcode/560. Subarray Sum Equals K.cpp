c1b17d7da410f4ea20d4c0080943da10dbf70918
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-05-05 23:30:28 +0530

    HashMap

---
+++ Leetcode/560. Subarray Sum Equals K.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+       
+        map<int,int> m;
+         int csum=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            csum += nums[i];
+            if(csum == k)
+            {
+                ans++;
+            }
+            if(m.find(csum-k) != m.end())
+            {
+                ans += m[csum-k];
+            }
+            
+            
+                m[csum]++;
+            
+        }
+        return ans;
+    }
+      
+};

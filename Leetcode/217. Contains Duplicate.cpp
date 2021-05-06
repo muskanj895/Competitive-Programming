26fcafd6d9b54b0e4b8af8d0e89f45abda433828
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-05-06 22:08:56 +0530

    Map & set

---
+++ Leetcode/217. Contains Duplicate.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       /* unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.find(nums[i]) != m.end())
+            {
+                return true;
+            }
+            m[nums[i]]++;
+        }
+        return false;*/
+        set<int> s;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(s.find(nums[i]) != s.end())
+            {
+                return true;
+            }
+            s.insert(nums[i]);
+        }
+        return false;
+    }
+};

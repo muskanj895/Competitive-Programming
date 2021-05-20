da9374d9713e245741d623f4bdf254eb00b178fc
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-05-20 17:52:45 +0530

    dp

---
+++ Leetcode/746. Min Cost Climbing Stairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size()+1,0);
+        for(int i=2;i<=cost.size();i++)
+        {
+            dp[i]=min(cost[i-2]+dp[i-2],cost[i-1]+dp[i-1]);
+        }
+        return dp[cost.size()];
+    }
+};

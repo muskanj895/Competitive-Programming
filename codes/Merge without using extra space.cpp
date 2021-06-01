d1b541d29245a93e6dd3b30f58fa8afd755f1826
Muskan Jain <63298380+muskanj895@users.noreply.github.com>
2021-06-01 16:11:30 +0530

    Create Merge without using extra space.cpp

---
+++ codes/Merge without using extra space.cpp
@@ -0,0 +1,180 @@
+//By using insertion sort
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void merge(int arr1[], int arr2[], int n, int m) {
+	   for(int i=m-1;i>=0;i--)
+	   {
+	       int j=n-2;
+	       int last=arr1[n-1];
+	       for(j=n-2;j>=0 && arr1[j]>arr2[i];j-- )
+	       arr1[j+1]=arr1[j];
+	       if(j != n-2 || last > arr2[i])
+	       {
+	           arr1[j+1]=arr2[i];
+	           arr2[i]=last;
+	       }
+	   }
+	    
+	        
+	    
+	    // code here
+	}
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, i;
+        cin >> n >> m;
+        int arr1[n], arr2[m];
+        for (i = 0; i < n; i++) {
+            cin >> arr1[i];
+        }
+        for (i = 0; i < m; i++) {
+            cin >> arr2[i];
+        }
+        Solution ob;
+        ob.merge(arr1, arr2, n, m);
+        for (i = 0; i < n; i++) {
+            cout << arr1[i] << " ";
+        }
+        for (i = 0; i < m; i++) {
+            cout << arr2[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
+
+
+//second 
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void merge(int arr1[], int arr2[], int n, int m) {
+	    int a1=0;
+	    int a2=0;
+	    while(a1 < n && a2 < m)
+	    {
+	        if(arr1[a1] > arr2[a2])
+	        {
+	        swap(arr1[a1],arr2[a2]);
+	        sort(arr2,arr2+m);
+	        a1++;
+	        }
+	        else
+	        {
+	            a1++;}
+	        
+	    }
+	    // code here
+	}
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, i;
+        cin >> n >> m;
+        int arr1[n], arr2[m];
+        for (i = 0; i < n; i++) {
+            cin >> arr1[i];
+        }
+        for (i = 0; i < m; i++) {
+            cin >> arr2[i];
+        }
+        Solution ob;
+        ob.merge(arr1, arr2, n, m);
+        for (i = 0; i < n; i++) {
+            cout << arr1[i] << " ";
+        }
+        for (i = 0; i < m; i++) {
+            cout << arr2[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
+//third method
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+int nextGap(int gap)
+{
+    if (gap <= 1)
+        return 0;
+    return (gap / 2) + (gap % 2);
+}
+	void merge(int arr1[], int arr2[], int n, int m) {
+	    int i, j, gap = n + m;
+    for (gap = nextGap(gap);
+         gap > 0; gap = nextGap(gap))
+    {
+        // comparing elements in the first array.
+        for (i = 0; i + gap < n; i++)
+            if (arr1[i] > arr1[i + gap])
+                swap(arr1[i], arr1[i + gap]);
+ 
+        // comparing elements in both arrays.
+        for (j = gap > n ? gap - n : 0;
+             i < n && j < m;
+             i++, j++)
+            if (arr1[i] > arr2[j])
+                swap(arr1[i], arr2[j]);
+ 
+        if (j < m) {
+            // comparing elements in the second array.
+            for (j = 0; j + gap < m; j++)
+                if (arr2[j] > arr2[j + gap])
+                    swap(arr2[j], arr2[j + gap]);
+        }
+    }
+	}
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, i;
+        cin >> n >> m;
+        int arr1[n], arr2[m];
+        for (i = 0; i < n; i++) {
+            cin >> arr1[i];
+        }
+        for (i = 0; i < m; i++) {
+            cin >> arr2[i];
+        }
+        Solution ob;
+        ob.merge(arr1, arr2, n, m);
+        for (i = 0; i < n; i++) {
+            cout << arr1[i] << " ";
+        }
+        for (i = 0; i < m; i++) {
+            cout << arr2[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
